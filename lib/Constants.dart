97938ee7d92c438199d71032931b09bfc31c4b38
RotenKiwi <joshiparth212@gmail.com>
2023-02-19 23:56:20 +0530

    Login screen start

---
+++ lib/Constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color nightPrimary = Color(0xff313745);
+const Color dayPrimary = Color(0xffe5ecf4);
---
+++ lib/Screens/LoginScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: size.height*0.23),
+          Text('Weather Check'),
+        ],
+      ),
+    );
+  }
+}
